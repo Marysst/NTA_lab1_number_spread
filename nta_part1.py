8a557f356c2fba5171421f304a9909c25a4e4fab
julia-20n <yulinima-ipt27@lll.kpi.ua>
2025-03-23 10:40:00 +0200

    Update nta_part1.py
    Додала функцію решета Ератосфена для генерації простих чисел

--- nta_part1.py
+++ nta_part1.py
@@ -40,3 +40,15 @@ def solovay_strassen(n, k=5):
             return False  
 
     return True  
+    
+def sieve(limit):
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False
+    primes = []
+    
+    for num in range(2, limit + 1):
+        if is_prime[num]:
+            primes.append(num)
+            for multiple in range(num * num, limit + 1, num):
+                is_prime[multiple] = False
+    return primes
