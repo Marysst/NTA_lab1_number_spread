3ea7d0287db766247124c49d168c3e7ba3989d30
julia-20n <yulinima-ipt27@lll.kpi.ua>
2025-03-23 10:03:43 +0200

    Update nta_part1.py
    Додала функцію для символу Лежандра, яка необхідна для імовірнісного тесту Соловея-Штрассена

--- nta_part1.py
+++ nta_part1.py
@@ -5,3 +5,16 @@ def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+
+def legendre_symbol(a, p):
+    if p <= 0 or p % 2 == 0:
+        raise ValueError("p має бути непарним простим числом")
+
+    a = a % p
+    if a == 0:
+        return 0
+    if a == 1:
+        return 1
+    if a % 2 == 0:
+        return legendre_symbol(a // 2, p) * (-1 if (p % 8 in (3, 5)) else 1)
+    return legendre_symbol(p % a, a) * (-1 if (a % 4 == 3 and p % 4 == 3) else 1)
