e8d7d24ff52ab2cef8d2cb1cdab5c8fdef3f0011
julia-20n <yulinima-ipt27@lll.kpi.ua>
2025-03-23 10:17:38 +0200

    Update nta_part1.py
    Додала ймовірнісний тест Соловея-Штрассена

--- nta_part1.py
+++ nta_part1.py
@@ -18,3 +18,25 @@ def legendre_symbol(a, p):
     if a % 2 == 0:
         return legendre_symbol(a // 2, p) * (-1 if (p % 8 in (3, 5)) else 1)
     return legendre_symbol(p % a, a) * (-1 if (a % 4 == 3 and p % 4 == 3) else 1)
+
+def solovay_strassen(n, k=5):
+    if n < 2:
+        return False
+    if n in (2, 3):
+        return True
+    if n % 2 == 0:
+        return False
+
+    for _ in range(k):
+        a = random.randint(2, n - 1)
+        g = gcd(a, n)
+        if g > 1:
+            return False  
+
+        jacobian = legendre_symbol(a, n) % n
+        mod_exp = pow(a, (n - 1) // 2, n)
+
+        if jacobian != mod_exp:
+            return False  
+
+    return True  
