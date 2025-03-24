1ac67b3b30e4decee3bb21b4e2694d662623826d
Marysst <mariua22102004@gmail.com>
2025-03-24 18:45:48 +0200

    Added process_b_value for Brilhart_Maurice

--- Brilhart_Maurice.py
+++ Brilhart_Maurice.py
@@ -11,3 +11,26 @@ def build_factor_base(n, a=1 / 2 ** 0.5):
         if p == 2 and n % 2 == 1 or sympy.legendre_symbol(n, p) == 1:
             factor_base.append(p)
     return factor_base
+
+def process_b_value(b_2, n, factor_base, b_values, relations):
+    b2 = (b_2 ** 2) % n
+    if b2 > n // 2:
+        b2 -= n
+    original_b2 = b2
+    factorization = []
+    
+    for p in factor_base:
+        count = 0
+        if p == -1:
+            if b2 < 0:
+                b2 //= p
+                count += 1
+        else:
+            while b2 % p == 0:
+                b2 //= p
+                count += 1
+        factorization.append(count % 2)
+    
+    if b2 == 1 and b_2 not in b_values:
+        relations.append((b_2, original_b2, factorization))
+        b_values.add(b_2)
